f1c75c74df2721c0006474b4f5d44c0244ea886c
jyothikakalu <jyothika.kalugotla@gmail.com>
2024-05-06 17:33:28 +0530

    forget

--- src/App.js
+++ src/App.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Login from './pages/Login.jsx';
-import SignUp from './pages/Signup.jsx';
-import ForgotPass from './pages/ForgotPass.jsx';
-// import {Bingo} from "./pages/Bingo.jsx"
-// import {useSelector, useDispatch} from "react-redux"
-// import { setBoard, setStart } from './store/sclices/bingoSlice.js';
-
-function App() {
-
-  return (
-    <div className="App">
-     <Routes>
-      <Route path='/'element={<Login/>} ></Route>
-      <Route path='/signup' element={<SignUp/>}></Route>
-      <Route path='/forgot' element={<ForgotPass/>}></Route>
-     </Routes>
-    </div>
-  );
-}
-
-export default App;
-=======
 import React from 'react'
 import Homepage from './pages/Homepage.jsx'
 
@@ -36,4 +11,3 @@ const App = () => {
 }
 
 export default App
->>>>>>> c5519ab21544bec3a86e893fc337bc68cf5255bf
