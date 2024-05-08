3cfb1ffc7e67f68e82e06e3f9a174b7958963407
Ritik Sharma <ritiksharma1995@gmail.com>
2024-05-08 14:31:11 +0530

    code

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,141 +1,178 @@
-import { useDispatch, useSelector } from 'react-redux';
+import {useNavigate} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
 import React, { useEffect, useState } from 'react';
+import { Alert, Avatar, Box, Button, Checkbox, Container, createTheme, CssBaseline, FormControlLabel, Grid, Link, Snackbar, TextField, ThemeProvider, Typography } from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import validator from 'validator';
-import { useNavigate } from 'react-router-dom';
-import { Button } from '@mui/material';
 
-export default function Login() {
+function Copyright(props) {
+    return (
+      <Typography variant="body2" color="text.secondary" align="center" {...props}>
+        {'Copyright © '}
+        <Link color="inherit"  target='_blank'>
+          Shopspot
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
+  
+  const theme = createTheme();
+
+export default function Login(){
     const [errors, setErrors] = useState({});
     const user = useSelector(state => state.user);
     const [openFailure, setOpenFailure] = useState(false);
-    const [openPasswordFailure, setOpenPasswordFailure] = useState(false);
+    const [openPasswordFailure, setOpenPasswordFailure]= useState(false);
     const isLoggedIn = Object.keys(user).length !== 0;
-    const navigate = useNavigate();
+    const navigate = useNavigate()
     const dispatch = useDispatch();
 
     useEffect(() => {
-        if (isLoggedIn) {
+        if(isLoggedIn) {
             navigate('/');
         }
     });
-
     const handleSubmit = (event) => {
         event.preventDefault();
         const data = new FormData(event.currentTarget);
         const formData = {};
-        const newErrors = { ...errors };
-        for (let [name, value] of data) {
-            formData[name] = value;
-            validate(name, value, newErrors);
+        const newErrors = {...errors};
+        for(let [name, value] of data) {
+          formData[name] = value;
+          validate(name, value, newErrors);
         }
-        if (Object.keys(newErrors).length !== 0) {
-            setErrors(newErrors);
-            return;
+        if(Object.keys(newErrors).length !== 0) {
+          setErrors(newErrors);
+          return;
         }
         const userData = JSON.parse(localStorage.getItem(formData.email));
-        if (userData === null) {
+        if(userData === null) {
             setOpenFailure(true);
             return;
         }
-        if (userData.password !== formData.password) {
-            setOpenPasswordFailure(true);
-            return;
+        if(userData.password !== formData.password) {
+          setOpenPasswordFailure(true);
+          return;
         }
-        dispatch({ type: 'login', payload: userData });
-    };
+        dispatch({type: 'login', payload: userData});
+      };
 
-    const validateComponent = (event) => {
-        const newErrors = validate(event.currentTarget.name, event.currentTarget.value, { ...errors });
+      const validateComponent = (event) => {
+        const newErrors = validate(event.currentTarget.name, event.currentTarget.value, {...errors});
         setErrors(newErrors);
-    }
+      }
 
-    const validate = (name, value, newErrors) => {
+      const validate = (name, value, newErrors) => {
         let error = '';
-        if (!value) {
+        if(!value) {
             error = 'Value required!'
         } else {
-            switch (name) {
+            switch(name) {
                 case 'email':
-                    if (!validator.isEmail(value)) {
+                    if(!validator.isEmail(value)) {
                         error = 'Please enter a proper Email ID'
                     }
                     break;
             }
         }
-        if (error === '') {
+        if(error === '') {
             delete newErrors[name]
         } else {
             newErrors[name] = error;
         }
         return newErrors;
-    }
+      }
 
-    if (openFailure) {
+      if(openFailure) {
         setTimeout(() => {
-            setOpenFailure(false);
+          setOpenFailure(false);
         }, 2000);
-    }
-    if (openPasswordFailure) {
+      }
+      if(openPasswordFailure) {
         setTimeout(() => {
-            setOpenPasswordFailure(false);
+          setOpenPasswordFailure(false);
         }, 2000);
-    }
-
-    const handleForgetPassword = () => {
-        navigate('/Forgetpass');
-    }
-
-    return (
-        <div className="min-h-screen flex justify-center items-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
-            <div className="max-w-md w-full space-y-8">
-                <div>
-                    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
-                </div>
-                <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
-                    <div className="rounded-md shadow-sm -space-y-px">
-                        <div>
-                            <label htmlFor="email-address" className="sr-only">Email address</label>
-                            <input id="email-address" name="email" type="email" autoComplete="email" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address" onChange={validateComponent} />
-                            {errors.email && <p className="mt-2 text-sm text-red-600">{errors.email}</p>}
-                        </div>
-                        <div>
-                            <label htmlFor="password" className="sr-only">Password</label>
-                            <input id="password" name="password" type="password" autoComplete="current-password" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password" onChange={validateComponent} />
-                            {errors.password && <p className="mt-2 text-sm text-red-600">{errors.password}</p>}
-                        </div>
-                    </div>
-
-                    <div className="flex items-center justify-between">
-                        <div className="flex items-center">
-                            <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" />
-                            <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">Remember me</label>
-                        </div>
-
-                        <div className="text-sm">
-                            <Button onClick={handleForgetPassword} className="font-medium text-indigo-600 hover:text-indigo-500">
-                                Forgot your password?
-                            </Button>
-                        </div>
-                    </div>
-
-                    <div>
-                        <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                                <svg className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                    <path fillRule="evenodd" d="M10 3.75a.75.75 0 01.75.75v2.5a.75.75 0 01-1.5 0v-2.5a.75.75 0 01.75-.75z" clipRule="evenodd" />
-                                    <path fillRule="evenodd" d="M4.75 10a.75.75 0 00-.75.75v3a.75.75 0 001.5 0v-3A.75.75 0 004.75 10z" clipRule="evenodd" />
-                                    <path fillRule="evenodd" d="M15.25 10a.75.75 0 01.75.75v3a.75.75 0 01-1.5 0v-3a.75.75 0 01.75-.75z" clipRule="evenodd" />
-                                    <path fillRule="evenodd" d="M5.553 2.447a.75.75 0 011.061 1.06L4.561 9h14.878a.75.75 0 010 1.5H4.561l1.053 5.493a.75.75 0 11-1.422.4L3.14 10.5a.75.75 0 010-1.414l1.052-5.497a.75.75 0 01.401-.392z" clipRule="evenodd" />
-                                </svg>
-                            </span>
-                            Sign in
-                        </button>
-                    </div>
-                </form>
-                <div className="text-center">
-                    <p className="text-sm text-gray-600">Don't have an account? <a href="/signup" className="font-medium text-indigo-600 hover:text-indigo-500">Sign up</a></p>
-                </div>
-            </div>
-        </div>
-    );
+      }
+      return (
+        <ThemeProvider theme={theme}>
+          <Snackbar open={openFailure} autoHideDuration={6000}>
+            <Alert severity="error" sx={{ width: '100%' }}>
+              User not found
+            </Alert>
+          </Snackbar>
+          <Snackbar open={openPasswordFailure} autoHideDuration={6000}>
+            <Alert severity="error" sx={{ width: '100%' }}>
+              The password that is entered is wrong
+            </Alert>
+          </Snackbar>
+          <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+              sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Sign in
+              </Typography>
+              <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                  onChange={validateComponent}
+                  error={errors.hasOwnProperty('email')}
+                  helperText={errors.email}
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  onChange={validateComponent}
+                  error={errors.hasOwnProperty('password')}
+                  helperText={errors.password}
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Sign In
+                </Button>
+                <Grid container>
+                  <Grid item xs>
+                    <Link href="/signup" variant="body2">
+                        Don't have an account? Sign Up
+                    </Link>
+                  </Grid>
+                </Grid>
+              </Box>
+            </Box>
+            <Copyright sx={{ mt: 8, mb: 4 }} />
+          </Container>
+        </ThemeProvider>
+      );
 }
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,138 +1,245 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import validator from 'validator';
+import { Alert, Snackbar } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
 
 export default function SignUp() {
-    const [errors, setErrors] = useState({});
-    const [openSuccess, setOpenSuccess] = useState(false);
+
+    const [errors, setErrors] = React.useState({});
+    const [openSuccess, setOpenSuccess] = React.useState(false);
     const user = useSelector(state => state.user);
     const isLoggedIn = Object.keys(user).length !== 0;
-    const navigate = useNavigate();
+    const navigate = useNavigate()
 
-    useEffect(() => {
-        if (isLoggedIn) {
+    React.useEffect(() => {
+        if(isLoggedIn) {
+            console.log('reached');
             navigate('/');
         }
     });
-   
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const formData = new FormData(event.target);
-        const newErrors = { ...errors };
 
-        for (let [name, value] of formData) {
-            validate(name, value, newErrors);
-        }
-
-        if (Object.keys(newErrors).length !== 0) {
-            setErrors(newErrors);
-            return;
-        }
-
-        localStorage.setItem(formData.get('email'), JSON.stringify(Object.fromEntries(formData)));
-        setOpenSuccess(true);
-    };
-
-    const validateComponent = (event) => {
-        const newErrors = validate(event.target.name, event.target.value, { ...errors });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const formData = {};
+    const newErrors = {...errors};
+    for(let [name, value] of data) {
+        formData[name] = value;
+        validate(name, value, newErrors);
+    }
+    if(Object.keys(newErrors).length !== 0) {
         setErrors(newErrors);
-    };
-
-    const validate = (name, value, newErrors, formData) => {
-      let error = '';
-  
-      if (!value) {
-          error = 'Value required!';
-      } else {
-          switch (name) {
-              case 'email':
-                  if (!validator.isEmail(value)) {
-                      error = 'Please enter a proper Email ID';
-                  }
-                  break;
-              case 'password':
-                  if (value.length < 8) {
-                      error = 'Password should have at least 8 characters';
-                  }
-                  break;
-              case 'confirmPassword':
-                  const password = formData.get('password');
-                  if (password !== value) {
-                      error = "Doesn't match with password";
-                  }
-                  break;
-              case 'contactNumber':
-                  if (!validator.isMobilePhone(value)) {
-                      error = 'Please enter a proper contact number';
-                  }
-                  break;
-              default:
-                  break;
-          }
-      }
-  
-      if (error === '') {
-          delete newErrors[name];
-      } else {
-          newErrors[name] = error;
-      }
-  
-      return newErrors;
+        return;
+    }
+    localStorage.setItem(formData.email, JSON.stringify(formData));
+    setOpenSuccess(true);
   };
-  
 
-    useEffect(() => {
-        if (openSuccess) {
+  const validateComponent = (event) => {
+    const newErrors = validate(event.currentTarget.name, event.currentTarget.value, {...errors});
+    setErrors(newErrors);
+  }
+
+  const validate = (name, value, newErrors) => {
+    let error = '';
+    if(!value) {
+        error = 'Value required!'
+    } else {
+        switch(name) {
+            case 'email':
+                if(!validator.isEmail(value)) {
+                    error = 'Please enter a proper Email ID'
+                }
+                break;
+            case 'password':
+                if(value.length < 8) {
+                    error = 'Password should have atleast 8 characters'
+                }
+                break;
+            case 'confirmPassword':
+                if(document.getElementById('password').value !== value) {
+                    error = "Doesn't match with password"
+                }
+                break;
+            case 'contactNumber':
+                if(!validator.isMobilePhone(value)) {
+                    error = 'Please enter a proper contact number'
+                }
+            break;
+        }
+    }
+    if(error === '') {
+        delete newErrors[name]
+    } else {
+        newErrors[name] = error;
+    }
+    return newErrors;
+  }
+
+    React.useEffect(() => {
+        if(openSuccess) {
             setTimeout(() => {
                 navigate('/login');
             }, 2000);
         }
     }, [openSuccess]);
 
-    return (
-        <div className="min-h-screen flex justify-center items-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
-            <div className="max-w-md w-full space-y-8">
-                <div>
-                    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign up</h2>
-                </div>
-                <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
-                    <div className="rounded-md shadow-sm -space-y-px">
-                        <div>
-                            <input name="firstName" type="text" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="First Name" onChange={validateComponent} />
-                            {errors.firstName && <p className="mt-2 text-sm text-red-600">{errors.firstName}</p>}
-                        </div>
-                        <div>
-                            <input name="lastName" type="text" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Last Name" onChange={validateComponent} />
-                            {errors.lastName && <p className="mt-2 text-sm text-red-600">{errors.lastName}</p>}
-                        </div>
-                        <div>
-                            <input name="email" type="email" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email Address" onChange={validateComponent} />
-                            {errors.email && <p className="mt-2 text-sm text-red-600">{errors.email}</p>}
-                        </div>
-                        <div>
-                            <input name="password" type="password" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password" onChange={validateComponent} />
-                            {errors.password && <p className="mt-2 text-sm text-red-600">{errors.password}</p>}
-                        </div>
-                        <div>
-                            <input name="confirmPassword" type="password" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Confirm Password" onChange={validateComponent} />
-                            {errors.confirmPassword && <p className="mt-2 text-sm text-red-600">{errors.confirmPassword}</p>}
-                        </div>
-                        <div>
-                            <input name="contactNumber" type="tel" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Contact Number" onChange={validateComponent} />
-                            {errors.contactNumber && <p className="mt-2 text-sm text-red-600">{errors.contactNumber}</p>}
-                        </div>
-                    </div>
-                    <div>
-                        <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            Sign Up
-                        </button>
-                    </div>
-                </form>
-                <div className="text-center">
-                    <p className="text-sm text-gray-600">Already have an account? <a href="/login" className="font-medium text-indigo-600 hover:text-indigo-500">Sign in</a></p>
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <Snackbar open={openSuccess} autoHideDuration={6000}>
+        <Alert severity="success" sx={{ width: '100%' }}>
+          User signed up successfully!
+        </Alert>
+      </Snackbar>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label={"First Name"}
+                  onChange={validateComponent}
+                  error={errors.hasOwnProperty('firstName')}
+                  helperText={errors.firstName}
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                  onChange={validateComponent}
+                  error={errors.hasOwnProperty('lastName')}
+                  helperText={errors.lastName}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  onChange={validateComponent}
+                  error={errors.hasOwnProperty('email')}
+                  helperText={errors.email}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  onChange={validateComponent}
+                  error={errors.hasOwnProperty('password')}
+                  helperText={errors.password}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="confirmPassword"
+                  label="Confirm Password"
+                  type="password"
+                  id="confirmPassword"
+                  autoComplete="new-password"
+                  onChange={validateComponent}
+                  error={errors.hasOwnProperty('confirmPassword')}
+                  helperText={errors.confirmPassword}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="contactNumber"
+                  label="Contact Number"
+                  type="phone"
+                  id="contactNumber"
+                  autoComplete="phone"
+                  onChange={validateComponent}
+                  error={errors.hasOwnProperty('contactNumber')}
+                  helperText={errors.contactNumber}
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
+  );
 }
